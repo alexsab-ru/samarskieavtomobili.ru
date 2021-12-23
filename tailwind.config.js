5333b8ae6b72462b4a2f928b25deab4f9a3267e7
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2021-12-23 14:44:25 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+	mode: 'jit',
+
+	purge: ['./dist/**/*.html'],
+
+	darkMode: false, // or 'media' or 'class'
+
+	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+		},
+		colors: {
+			mainBlue: '#002C5F',
+			secondBlue: '#00AAD2',
+			sand: '#E4DCD3',
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+			white: colors.white,
+			gray: colors.trueGray,
+			indigo: colors.indigo,
+			red: colors.rose,
+			yellow: colors.amber,
+		},
+		extend: {
+			fontFamily: {
+				sans: ['hyunday-st', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+}
