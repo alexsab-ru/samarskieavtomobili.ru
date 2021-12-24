d70356a5dbbf8892900c2003038e524f1906b998
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2021-12-24 22:04:22 +0500

    about page content

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -2,22 +2,64 @@ import Swiper, {Lazy, Pagination, Navigation, EffectFade} from 'swiper';
 
 Swiper.use([Lazy, Pagination, Navigation]);
 
-const bannerSlider = new Swiper('.banner-slider', {
-	loop: true,
-	slidesPerView: 1,
-	speed: 600,
-	preloadImages: false,
-	lazy: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	pagination: {
-		el: '.swiper-pagination',
+let bannerSlider;
+let loop = false;
+
+const initSlider = (num = 0, loop) => {
+	bannerSlider = new Swiper('.banner-slider', {
+		loop: loop,
+		slidesPerView: 1,
+		speed: 500,
+		preloadImages: false,
+		lazy: true,
+		initialSlide: num,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
 			type: 'bullets', //'bullets' | 'fraction' | 'progressbar' | 'custom'
 			clickable: true,
-	},
-	breakpoints: {
-		
+		},
+		breakpoints: {
+			320: {
+				pagination: false
+			},
+			580: {
+				pagination: {
+					el: '.swiper-pagination',
+					type: 'bullets', //'bullets' | 'fraction' | 'progressbar' | 'custom'
+					clickable: true,
+				},
+			},
 		}
-})
+	})
+}
+
+const slides = document.querySelectorAll('.banner-slide');
+
+if (slides.length > 1) loop = true;
+
+initSlider(0, loop);
+
+
+// const bannerSlider = new Swiper('.banner-slider', {
+// 	loop: true,
+// 	slidesPerView: 1,
+// 	speed: 600,
+// 	preloadImages: false,
+// 	lazy: true,
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 			type: 'bullets', //'bullets' | 'fraction' | 'progressbar' | 'custom'
+// 			clickable: true,
+// 	},
+// 	breakpoints: {
+		
+// 		}
+// })
