44feaf69d47af146d86b9eb32ec05432ce226a59
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2021-12-27 22:56:24 +0500

    mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,18 @@ window.Alpine = Alpine
 Alpine.start()
 
 import './modules/slider'
+
+function asidePos(){
+	let WW = window.innerWidth;
+	if(WW < 1024){
+		let CW = document.querySelector('.container').clientWidth;
+		let right = (WW - CW) / 2;
+		const $aside = document.querySelector('aside');
+		$aside.style.right = right + 'px';
+	}
+}
+asidePos()
+
+window.addEventListener('resize', function(){
+	asidePos()
+})
