42b40c7e41b905f8a94453614d1746731cb5f965
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-05-17 22:45:48 +0500

    Create types.ts

---
+++ src/components/Banner/types.ts
@@ -0,0 +1,16 @@
+export type TBanner = {
+	show?: boolean
+	type?: string
+	view?: string
+	videoUrl?: string
+	imageUrl: string
+	bannerUrl?: string
+	target?: string
+	title?: string
+	descr?: string
+	btnTitle?: string
+	btnUrl?: string
+	modalTitle?: string,
+	brandValue?: string,
+	autoplay?: number
+}
